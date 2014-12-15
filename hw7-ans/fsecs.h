f3d2a5d08d367ea9347ce0260e5164dcd50f0863
kathrynsiegel <ksiegel@mit.edu>
2014-12-15 18:03:58 -0500

    cleaning up code

--- hw7-ans/fsecs.h
+++
@@ -1,9 +0,0 @@
-#ifndef MM_FSECS_H
-#define MM_FSECS_H
-
-typedef void (*fsecs_test_funct)(void *);
-
-void init_fsecs(void);
-double fsecs(fsecs_test_funct f, void *argp);
-
-#endif /* MM_FSECS_H */
